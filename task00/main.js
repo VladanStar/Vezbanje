45bb3137cec6c6d115f4a16cf263b82d68f91830
VladanStar <vladancupric@gmail.com>
2023-01-09 22:24:43 +0100

    Add

--- task00/main.js
+++ task00/main.js
@@ -5,20 +5,20 @@
 //   .then((data) => console.log(data));
 
 function renderWeather(weather) {
-  let query = document.getElementsByClassName('city').value;
-  console.log(weather)
+  // let query = document.getElementsByClassName("city").value;
+  console.log(weather);
   let resultsContainer = document.getElementById("weather-result");
   // create h2 for name
-  let city = document.createElement('h2');
+  let city = document.createElement("h2");
   city.textContent = weather.city.name;
   resultsContainer.append(city);
   // create p for humidity, wind, dwscription, temp
 
   let temp = document.createElement("p");
   temp.textContent = "Temp: " + weather.list[0].temp.day + "F";
-  resultsContainer.append(temp)
-  
-  let humidity =  document.createElement("p");
+  resultsContainer.append(temp);
+
+  let humidity = document.createElement("p");
   humidity.textContent = "Humidity: " + weather.list[0].humidity;
   resultsContainer.append(humidity);
 
@@ -27,25 +27,28 @@ function renderWeather(weather) {
   resultsContainer.append(wind);
 
   var weatherDetails = weather.list[0].weather[0].id;
-  if(weatherDetails &&  weatherDetails.description){
-    let descriptions= document.createElement("p");
+  if (weatherDetails && weatherDetails.description) {
+    let descriptions = document.createElement("p");
     descriptions.textContent = weatherDetails.descriptions;
-    resultsContainer.append(descriptions);  
+    resultsContainer.append(descriptions);
   }
 
   // details.append("")
 }
 
 // Fetch weather data for city
-function fetchWeather(query){
-  let url = "https://api.openweathermap.org/data/2.5/forecast/daily?q="+ query + "&mode=json&appid=1b1d0ad1ab386b48e8311d1e61b27554";
+function fetchWeather(query) {
+  let url =
+    "https://api.openweathermap.org/data/2.5/forecast/daily?q=" +
+    text +
+    "&mode=json&appid=1b1d0ad1ab386b48e8311d1e61b27554";
 
-   fetch(url)
- .then((response) => response.json())
-  .then((data) => renderWeather(data))
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderWeather(data));
 }
-let query = document.getElementsByClassName('city');
+let query = document.getElementById("city");
 
-var value = query.value;
-var text = query.options[query.selectedIndex].text
-fetchWeather(query);
+// var value = query.value;
+var text = query.options[query.selectedIndex].text;
+fetchWeather("text");
